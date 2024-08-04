3d606c3510740154e7d52fda9c7f4b249a8a27a8
syedraza360 <77007376+syeddanial110@users.noreply.github.com>
2024-08-04 21:16:21 +0500

    ai image generation

--- src/util/ApiRequest.js
+++ src/util/ApiRequest.js
@@ -7,16 +7,21 @@ export const apiBaseUrl = `${BASEURL}`
 export function getCommonHeaders(h) {
   var headers = {
     // 'Content-Type': 'application/json',
-    'x-api-key': 'SG_0eddaa02fce10569',
+    'x-api-key': `${process.env.NEXT_PUBLIC_SECRET_KEY}`,
   }
   return headers
 }
 
 export function apiPost(endpoint, body, onSuccess, onFailure, headers) {
   axios
-    .post(endpoint, body, {
-      headers: getCommonHeaders(headers),
-    }, { responseType: "arraybuffer" } )
+    .post(
+      endpoint,
+      body,
+      {
+        headers: getCommonHeaders(headers),
+      },
+      { responseType: 'arraybuffer' },
+    )
     .then((response) => {
       onSuccess(response)
     })
